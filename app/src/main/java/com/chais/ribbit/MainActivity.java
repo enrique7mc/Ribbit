647a76184b0b393ccbfd33cb517961ec1305fd9e
enrique7mc <enrique7mc@gmail.com>
2015-05-10 16:43:28 -0500

    Add sending messages

--- app/src/main/java/com/chais/ribbit/MainActivity.java
+++ app/src/main/java/com/chais/ribbit/MainActivity.java
@@ -100,6 +100,17 @@ public class MainActivity extends ActionBarActivity {
 				mediaScanIntent.setData(mMediaUri);
 				sendBroadcast(mediaScanIntent);
 			}
+
+			Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
+			recipientsIntent.setData(mMediaUri);
+			String fileType;
+			if (requestCode == PICK_PHOTO_REQUEST || requestCode == TAKE_PHOTO_REQUEST) {
+				fileType = ParseConstants.TYPE_IMAGE;
+			} else {
+				fileType = ParseConstants.TYPE_VIDEO;
+			}
+			recipientsIntent.putExtra(ParseConstants.KEY_FILE_TYPE, fileType);
+			startActivity(recipientsIntent);
 		} else if (resultCode != RESULT_CANCELED) {
 			Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();
 		}
@@ -138,7 +149,6 @@ public class MainActivity extends ActionBarActivity {
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 		startActivity(intent);
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/chais/ribbit/SignUpActivity.java
+++ app/src/main/java/com/chais/ribbit/SignUpActivity.java
@@ -35,29 +35,6 @@ public class SignUpActivity extends ActionBarActivity {
 		ButterKnife.inject(this);
 	}
 
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_signup, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
 	@OnClick(R.id.signupButton)
 	public void SignUpButtonClick(View view) {
 		String username = mUsername.getText().toString();
