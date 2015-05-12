7566f97fd5e50923bfe201dbee031fa42e4507aa
enrique7mc <enrique7mc@gmail.com>
2015-05-11 19:22:54 -0500

    Add check for network
    Login & Sign Up

--- app/src/main/java/com/chais/ribbit/activities/LoginActivity.java
+++ app/src/main/java/com/chais/ribbit/activities/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.chais.ribbit.activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -22,12 +24,13 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
 	private static final String TAG = LoginActivity.class.getSimpleName();
 	@InjectView(R.id.usernameField) EditText mUsername;
 	@InjectView(R.id.passwordField) EditText mPassword;
 	@InjectView(R.id.loginButton) Button mLoginButton;
 	@InjectView(R.id.signUpText) TextView mSignUpTextView;
+	ProgressDialog progress;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,11 @@ public class LoginActivity extends ActionBarActivity {
 
 	@OnClick(R.id.loginButton)
 	public void SignUpButtonClick(View view) {
+		if (!Util.isNetworkAvailable(this)) {
+			Util.alertDialogShow(this, getString(R.string.general_error),
+					getString(R.string.no_network_available_message));
+			return;
+		}
 		String username = mUsername.getText().toString();
 		String password = mPassword.getText().toString();
 
@@ -58,14 +66,15 @@ public class LoginActivity extends ActionBarActivity {
 			return;
 		}
 
-		setSupportProgressBarIndeterminateVisibility(true);
+		progress = ProgressDialog.show(this, "Logging in",
+				"Please wait a moment", true);
 		ParseUser.logInInBackground(username, password, logInCallback);
 	}
 
 	private LogInCallback logInCallback = new LogInCallback() {
 		@Override
 		public void done(ParseUser parseUser, ParseException e) {
-			setSupportProgressBarIndeterminateVisibility(false);
+			progress.dismiss();
 			if (e != null) {
 				Log.e(TAG, e.getMessage());
 				String message = e.getMessage().equals("invalid login parameters") ?
--- app/src/main/java/com/chais/ribbit/activities/MainActivity.java
+++ app/src/main/java/com/chais/ribbit/activities/MainActivity.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
